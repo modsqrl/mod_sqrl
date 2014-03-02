781f06009d1910b1eff721465500a1318c57f338
Chris Steinhoff <cs.otherstuff@gmail.com>
2014-03-02 09:03:41 -0700

    Start of autotools.

--- src/main/c/modules.mk
+++
@@ -1,6 +0,0 @@
-mod_sqrl.la: mod_sqrl.slo sqrl.slo sqrl_encodings.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_sqrl.lo sqrl.lo sqrl_encodings.lo
-
-DISTCLEAN_TARGETS = modules.mk
-shared =  mod_sqrl.la
-
